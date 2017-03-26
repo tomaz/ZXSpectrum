e995929a4dcd856e0e89fa53d02a25774011ae88
Tomaz Kragelj <tkragelj@gmail.com>
2017-03-26 16:47:24 +0200

    Initial project files.

---
+++ ZX Spectrum/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZX Spectrum
+//
+//  Created by Tomaz Kragelj on 26.03.17.
+//  Copyright © 2017 Gentle Bytes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
