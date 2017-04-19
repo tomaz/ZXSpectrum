49d942e90ab567f91f6545ec694f7c33ff5d5d74
Tomaz Kragelj <tkragelj@gmail.com>
2017-04-19 14:04:20 +0200

    Toolbar view is more pronounced.

--- ZX Spectrum/View/ToolbarView.swift
+++ ZX Spectrum/View/ToolbarView.swift
@@ -11,8 +11,35 @@ Toolbar container view.
 Use as container for "toolbar" controls; it'll take care of intrinsic content size.
 */
 final class ToolbarView: UIView {
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		initializeView()
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		initializeView()
+	}
+	
+	private func initializeView() {
+		backgroundColor = UIColor.lightGray.withAlphaComponent(0.25)
+	}
+	
+	// MARK: - Overriden functions
 
 	override var intrinsicContentSize: CGSize {
 		return CGSize(width: UIViewNoIntrinsicMetric, height: 44)
+	}
+	
+	override func draw(_ rect: CGRect) {
+		super.draw(rect)
+		
+		let path = UIBezierPath()
+		path.move(to: CGPoint(x: 0, y: 0))
+		path.addLine(to: CGPoint(x: bounds.maxX, y: 0))
+		
+		UIColor.lightGray.setStroke()
+		path.stroke()
 	}
 }
